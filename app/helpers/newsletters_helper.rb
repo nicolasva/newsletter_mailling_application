f8c296040c700b6e4a7cea61827e5329518ce286
VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2011-10-30 20:37:19 +0100

    update version

--- app/helpers/newsletters_helper.rb
+++ app/helpers/newsletters_helper.rb
@@ -14,15 +14,15 @@ module NewslettersHelper
    					<td class='td_class_title' id='id_td_title_newsletter' width='450'>#{label_tag(Newsletter.human_attribute_name("name"))}</td>
     					<td class='td_class_title_edition'>#{label_tag(t("common_link.show"))}</td>
     					<td class='td_class_title_edition'>#{label_tag(t("common_link.editing"))}</td>
-    					<td class='td_class_title_edition_last'>#{label_tag(t("common_link.delete"))}</td>
+    					<td class='td_class_title_edition_last'>#{label_tag(t("common_link.removal"))}</td>
   				</tr>"
 
 				newsletters.each do |newsletter|
   				newsletters_lists += "<tr>
     					<td class='td_class_newsletter_name'>#{newsletter.name}</td>
     					<td>#{link_to t(".show_newsletter"), newsletter}</td>
-    					<td>#{link_to 'Editer', edit_newsletter_path(newsletter)}</td>
-    					<td class='td_class_last_column'>#{link_to 'Supprimer', newsletter, :confirm => 'Are you sure?', :method => :delete }</td>
+    					<td>#{link_to image_tag(t("common_link.edit"), :size=>"18x18"), edit_newsletter_path(newsletter)}</td>
+    					<td class='td_class_last_column'>#{link_to image_tag(t("common_link.delete")), newsletter, :confirm => 'Are you sure?', :method => :delete }</td>
   				</tr>"
 				end
 			newsletters_lists += "</table>"

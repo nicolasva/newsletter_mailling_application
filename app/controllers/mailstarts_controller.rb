d4cff6f79e1442c2734e28096319df50221c5ff4
VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2011-11-02 17:15:15 +0100

    update version

--- app/controllers/mailstarts_controller.rb
+++ app/controllers/mailstarts_controller.rb
@@ -43,7 +43,7 @@ class MailstartsController < ApplicationController
     @mailstart = Mailstart.find(params[:id])
     @categoryall = @mailstart.categoryalls.nil? ? "" : @mailstart.categoryalls.first
     @subcontact = @categoryall.nil? || @categoryall.subcontacts.nil? ? "" : @categoryall.subcontacts.first
-    @mail = @subcontact.nil? || @subcontact.empty? || @subcontact.mails.nil? ? "" : @subcontact.mails.first
+    @mail = @subcontact.nil? || @subcontact.mails.nil? ? "" : @subcontact.mails.first
     respond_with @mailstart
   end
 

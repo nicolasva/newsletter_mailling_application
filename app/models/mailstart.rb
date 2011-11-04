a6a1ec620ba65f675f61e31db5e72f1ea1e686f4
VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2011-11-04 14:36:22 +0100

    update version

--- app/models/mailstart.rb
+++ app/models/mailstart.rb
@@ -5,7 +5,7 @@ has_many :newsletters
 accepts_nested_attributes_for :categoryalls
 validates_presence_of :mail
 validates_presence_of :user_id
-attr_accessible :name, :mail
+attr_accessible :name, :mail, :user_id, :position
 	def self.position(index,id)
 		#update_all(['position = FIND_IN_SET(id, ?)', ids.join(',')], { :id => ids})
 		update_all(['position=?', index], ['id=?', id])

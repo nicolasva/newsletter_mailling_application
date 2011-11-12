0ca26a36a7eab8309071e8d651a405574f917cc9
VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2011-11-12 20:40:25 +0100

    update version rspec test

--- spec/requests/mailstarts_spec.rb
+++ spec/requests/mailstarts_spec.rb
@@ -12,11 +12,28 @@ describe "Mailstarts" do
 	      page.should have_link("Creer un nouveau carnet d'adresse", :href => new_mailstart_path)
 	end
 
+	it "create mailstart" do
+	      visit mailstarts_path
+	      click_link "Creer un nouveau carnet d'adresse"
+	      page.should have_content("Adresse e-mail de départ")
+	end
+
 	it "edit mailstart" do
 	      visit edit_mailstart_path(@mailstart)
 	      page.should have_content("Adresse e-mail de départ")
 	      click_button "Enregistrer les informations concernant ce carnet d'adresse"
 	      page.should have_content("enregistrement concernant les information de ce carnet d'adresse s'est bien déroulé, elles sont désormais disponible")
-	     page.should have_content("Gestion du carnet d'adresse") 
+	      page.should have_content("Gestion du carnet d'adresse") 
+	end
+
+	it "click edit mailstart button add category subcontact mail" do
+	     visit edit_mailstart_path(@mailstart)
+	     click_button "Ajouter une nouvelle catégorie"
+	     page.should have_content("L'enregistrement concernant les information de ce carnet d'adresse s'est bien déroulé, elles sont désormais disponible")
+	     categoryall = Factory(:categoryall)
+	     click_button "Ajouter un nouveau sous-contact"
+	     page.should have_content("L'enregistrement concernant les information de ce carnet d'adresse s'est bien déroulé, elles sont désormais disponible")
+	     click_button "Ajouter une nouvelle adresse e-mail"
+	     page.should have_content("L'enregistrement concernant les information de ce carnet d'adresse s'est bien déroulé, elles sont désormais disponible")
 	end
 end
